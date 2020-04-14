7b9ac32df88c7c301f77ddacecf6399e52226213
Jay R Bolton <jayrbolton@gmail.com>
2020-04-14 16:23:55 -0700

    Fallback to a special appdev RE API url if we're in that environment

--- src/utils/config.py
+++ src/utils/config.py
@@ -7,8 +7,11 @@ import functools
 
 @functools.lru_cache(maxsize=1)
 def get_config():
+    re_url = os.environ.get('KBASE_SECURE_CONFIG_PARAM_RE_API_URL', 'http://re_api:5000').strip('/')
+    if 'appdev' in os.environ.get('KBASE_ENDPOINT', ''):
+        re_url = os.environ.get('KBASE_SECURE_CONFIG_PARAM_APPDEV_RE_API_URL').strip('/')
     config = {
-        're_url': os.environ.get('KBASE_SECURE_CONFIG_PARAM_RE_API_URL', 'http://re_api:5000').strip('/'),
+        're_url': re_url,
         'dev': 'DEVELOPMENT' in os.environ,
         'nworkers': os.environ.get('KBASE_SECURE_CONFIG_PARAM_NWORKERS', 2)
     }
